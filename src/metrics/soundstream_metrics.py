962d3ee2fb3f58357e47a10348d63bc4d4a3a0cd
Sergei Zaitsev <svzaitsev@edu.hse.ru>
2026-05-14 19:59:17 +0300

    Add metrics

---
+++ src/metrics/soundstream_metrics.py
@@ -0,0 +1,67 @@
+import torch
+from tochmetrics.audio import (
+    NonIntrusiveSpeechQualityAssessment,
+    ShortTimeObjectiveIntelligibility,
+)
+
+from src.metrics.base_metric import BaseMetric
+
+
+class STOIMetric(BaseMetric):
+    """
+    STOI metric.
+    """
+
+    def __init__(self, metric, device, *args, **kwargs):
+        """
+        Args:
+            metric (Callable): function to calculate metrics.
+            device (str): device for the metric calculation (and tensors).
+        """
+
+        super().__init__(*args, **kwargs)
+        if device == "auto":
+            device = "cuda" if torch.cuda.is_available() else "cpu"
+
+        self.metric = metric.to(device)
+
+    def __call__(self, audio, fake_audio, **kwargs):
+        """
+        Args:
+            audio (torch.Tensor): audio tensor of real audio (B, 1, T).
+            fake_audio (torch.Tensor): audio tensor of reconstructed audio (B, 1, T).
+        Returns:
+            stoi (float): STOI metric.
+        """
+        audio = audio.squeeze(1)
+        fake_audio = fake_audio.squeeze(1)
+
+        return self.metric(fake_audio, audio).mean()
+
+
+class NISQAMetric(BaseMetric):
+    """
+    NISQA metric.
+    """
+
+    def __init__(self, metric, device, *args, **kwargs):
+        """
+        Args:
+            metric (Callable): function to calculate metrics.
+            device (str): device for the metric calculation (and tensors).
+        """
+
+        super().__init__(*args, **kwargs)
+        if device == "auto":
+            device = "cuda" if torch.cuda.is_available() else "cpu"
+
+        self.metric = metric.to(device)
+
+    def __call__(self, fake_audio, **kwargs):
+        """
+        Args:
+            fake_audio (torch.Tensor): audio tensor of reconstructed audio (B, 1, T).
+        """
+        fake_audio = fake_audio.squeeze(1)
+
+        return self.metric(fake_audio).mean()
